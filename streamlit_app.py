55c0987b307455550c8b4d4d63535452c0960227
mt-pain <maekawa@pain-ml.com>
2023-11-02 16:17:05 +0900

    いろいろと改変

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,43 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
+# ...
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 4000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    
+    # 追加のUIコンポーネントをここに配置
+    color = st.color_picker('Choose a color for the spiral', '#0068c9')
+    shape = st.selectbox('Choose the shape of the points', ['circle', 'square', 'triangle', 'star'])
+    opacity = st.slider('Opacity of the spiral points', 0.0, 1.0, 0.5)
+    point_size = st.slider('Size of the points on the spiral', 1, 100, 10)
+    density = st.slider('Density of points on spiral', 0.1, 2.0, 1.0)
+    show_tooltip = st.checkbox('Show tooltip on hover')
 
     Point = namedtuple('Point', 'x y')
     data = []
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
+    # スパイラルの点を生成するループを更新
+    points_per_turn = total_points / (num_turns * density)
+    for curr_point_num in range(int(total_points * density)):
         curr_turn, i = divmod(curr_point_num, points_per_turn)
         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
+        radius = curr_point_num / (total_points * density)
         x = radius * math.cos(angle)
         y = radius * math.sin(angle)
         data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # アルタイチャートの描画部分を更新
+    chart = alt.Chart(pd.DataFrame(data), height=500, width=500).mark_point(
+        shape=shape,  # マークの形状を変更
+        color=color,  # マークの色を変更
+        opacity=opacity,  # マークの透明度を変更
+        size=point_size  # マークのサイズを変更
+    ).encode(
+        x='x:Q',
+        y='y:Q',
+        tooltip='index:N' if show_tooltip else None  # ツールチップの設定を追加
+    )
+
+    st.altair_chart(chart)
